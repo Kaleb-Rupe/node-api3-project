fb657527858490d1ee74707f94f9dce3955ed0d4
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-07-19 16:55:02 -0400

    Completed project MVP

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,63 @@
+const Posts = require("../posts/posts-model");
+const Users = require("../users/users-model");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleDateString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timestamp}] ${method} to ${url}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const user = await Users.getById(req.params.id);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      next({ status: 404, message: `user not found` });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const { name } = req.body;
+  if (
+    name !== undefined &&
+    typeof name === "string" &&
+    name.length &&
+    name.trim().length
+  ) {
+    next();
+  } else {
+    next({ status: 400, message: "missing required name field" });
+  }
 }
 
-function validatePost(req, res, next) {
+async function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const { text } = req.body;
+  if (!text || !text.trim()) {
+    res.status(400).json({
+      message: "missing required text field",
+    });
+  } else {
+    req.text = text.trim();
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+};
--- api/server.js
+++ api/server.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
+const usersRouter = require("./users/users-router");
+const { logger } = require("./middleware/middleware");
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
+server.use(express.json());
+server.use("/api/users", usersRouter);
 
-// global middlewares and the user's router need to be connected here
+server.use(logger);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.use("*", (req, res) => {
+  // catch all 404 errors middleware
+  res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
 });
 
 module.exports = server;
